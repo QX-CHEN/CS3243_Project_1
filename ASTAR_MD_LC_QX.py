8eebbbe4128aebae47539b38af47a6bcb480cdec
QX-CHEN <c.qixing@yahoo.com.sg>
2020-05-26 22:28:11 +0800

    fix ver2

--- ASTAR_MD_LC_QX.py
+++ ASTAR_MD_LC_QX.py
@@ -219,31 +219,37 @@ class Node(object):
                 right_row, right_col = right_position // self.dimension, right_position % self.dimension
                 # Linear conflict
                 conflict = 0
+
                 # Ver3 Complete LC : O(n^2) where n is the dimension
                 # if (i + 1) % self.dimension: # Exception last col
                 #     if curr_row == right_row:
-                #         conflict += sum(map(lambda x : self.state[i] > x and x != 0 and (x - 1) // self.dimension == curr_row, \
-                #              self.state[i + 1 : right_row * self.dimension + self.dimension]))
+                #         conflict += sum(map(lambda x : self.state[i] > x and x != 0 and (x - 1) // self.dimension == right_row, \
+                #              self.state[i + 1 : curr_row * self.dimension + self.dimension]))
                 # if i < self.dimension * (self.dimension - 1): # Except last row
                 #     if curr_col == right_col:
                 #         conflict += sum(map(lambda x : self.state[i] > self.state[x] and \
-                #         self.state[x] != 0 and (self.state[x] - 1) % self.dimension == curr_col, \
-                #         range(i + self.dimension, self.dimension * self.dimension, self.dimension)))
+                #         self.state[x] != 0 and (self.state[x] - 1) % self.dimension == right_col, \
+                #         range(i + self.dimension, self.dimension * (self.dimension - 1) + curr_col + 1, self.dimension)))
 
-                # Ver2 (WRONG)
+                # Ver2 Incomplete LC stronger than Ver1 : O(1)
+                # curr = self.state[i]
                 # if (i + 1) % self.dimension: # Exception last col
-                #     if curr_row == right_row and self.state[i] > self.state[i + 1] and self.state[i + 1] != 0:
+                #     right = self.state[i + 1]
+                #     if curr_row == right_row and curr > right and right != 0 and (right - 1) // self.dimension == right_row:
                 #         conflict += 1
                 # if i < self.dimension * (self.dimension - 1): # Except last row
-                #     if curr_col == right_col and self.state[i] > self.state[i + self.dimension] and self.state[i + self.dimension] != 0:
+                #     down = self.state[i + self.dimension]
+                #     if curr_col == right_col and curr > down and down != 0 and down % self.dimension == right_col:
                 #         conflict += 1
 
                 # Ver1 Incomplete LC : O(1)
                 if (i + 1) % self.dimension: # Exception last col
-                    if self.state[i] == (i + 2) and self.state[i + 1] == (i + 1):
+                    if self.state[i] == (i + 2) and self.state[i + 1] == (i + 1): 
+                    # right value for curr_pos = i + 1 + 1, next_pos = i + 1 + 1 - 1 = i + 1
                         conflict += 1
                 if i < self.dimension * (self.dimension - 1): # Except last row
-                    if self.state[i] == (i + 1 + self.dimension) and self.state[i + self.dimension] == (i + 1):
+                    if self.state[i] == (i + self.dimension + 1) and self.state[i + self.dimension] == (i + 1):
+                    # right value for curr_pos = i + dimension + 1, next_pos = i + dimension + 1 - dimension = i + 1
                         conflict += 1
 
                 score += (abs(curr_row - right_row) + abs(curr_col - right_col) + 2 * conflict)
